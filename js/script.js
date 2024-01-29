3ff29f99b397c781b959dabf7b55f5579b59529b
W1nt3rR <frostywinterr@gmail.com>
2024-01-29 01:01:24 +0100

    Update chat window dimensions

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function createChatInterface() {
     const chatButton = createElementWithProps("button", {
         id: "chatgpt-button",
         innerHTML: `
-            <svg fill="#000000" width="800px" height="800px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg">
+            <svg fill="#000000" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg">
                 <title>
                     OpenAI icon
                 </title>
